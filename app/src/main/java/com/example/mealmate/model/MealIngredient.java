4ead82fa64fde4b79dc562e9b4d6e56634f3f886
Mohamed Abdelrehim Abdelhady Aly <moabdelraheem19@gmail.com>
2024-08-19 15:11:20 +0300

    add button nav bar

---
+++ app/src/main/java/com/example/mealmate/model/MealIngredient.java
@@ -0,0 +1,7 @@
+package com.example.mealmate.model;
+
+public class MealIngredient {
+    public String idIngredient;
+    public String strIngredient;
+    public String strDescription;
+}
--- app/src/main/java/com/example/mealmate/veiw/main_activity/MainActivity.java
+++ app/src/main/java/com/example/mealmate/veiw/main_activity/MainActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.NavGraph;
 import androidx.navigation.Navigation;
 
 import com.example.mealmate.R;
-
+import com.example.mealmate.veiw.home_activity.HomeActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private NavController navController;
@@ -18,7 +18,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize NavController
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        // Check if the user is logged in
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            // User is logged in, navigate to the home screen
+            navigateToHomeScreen();
+        } else {
+            // User is not logged in, navigate to the login screen
+            navigateToLoginScreen();
+        }
 
         // Check if there is a destination fragment passed via the intent
         Intent intent = getIntent();
@@ -26,13 +37,25 @@ public class MainActivity extends AppCompatActivity {
             String destinationFragment = intent.getStringExtra("destination_fragment");
             navigateToFragment(destinationFragment);
         }
-
-
     }
 
     private void navigateToFragment(String fragmentName) {
         if ("loginFragment".equals(fragmentName)) {
             navController.navigate(R.id.loginFragment);
+        } else if ("homeFragment".equals(fragmentName)) {
+            navController.navigate(R.id.homeFragment);
         }
-        }
+        // Add more conditions for other fragments if needed
+    }
+
+    private void navigateToHomeScreen() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    private void navigateToLoginScreen() {
+        navController.navigate(R.id.loginFragment);
+    }
 }
